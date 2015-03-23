71057c6d674aa963e985c2e249ed2ee982adfbd0
Michael Schulz <ms@jocax.com>
2015-03-23 20:21:30 +0100

    Removed module client-angularjs

--- client-angularjs/src/main/resources/static/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var imageApp = angular.module('imageApp', ['infinite-scroll', 'ngResource']);
-
-imageApp.controller('ImageLoaderController', function($scope, ImageLoader) {
-    $scope.imageLoader = new ImageLoader();
-});
